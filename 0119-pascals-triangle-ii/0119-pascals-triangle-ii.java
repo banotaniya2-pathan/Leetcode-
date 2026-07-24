9b4915f47502ffa18188ab78249373c7601be31f
banotaniya2-pathan <banotaniya2@gmail.com>
2026-07-25 00:34:00 +0530

    Time: 0 ms (100%), Space: 42.4 MB (72.19%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+       List<Integer> ans = new ArrayList<>();
+        long res =1;
+        ans.add(1);
+        for(int i=0;i<rowIndex;i++){
+            res=res*(rowIndex-i);
+            res=res/(i+1);
+    
+        ans.add((int)res);
+        }
+        return ans;
+    }
+}     
